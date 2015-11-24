73fb4587b2eefa9ac0f2bd3ad7c199c7b9b4135b
Dan Winter <dan.j.winter@gmail.com>
2015-11-24 12:23:47 -0700

    finished extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,51 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-num = rand(100)
+number = rand(100)
+
+def check_guess(guess, number)
+  if guess > number
+    if guess > number + 5
+      "Way too high!"
+    else
+    "Too high!"
+  end
+  elsif guess < number
+    if guess < number - 5
+      "Way too low!"
+    else
+    "Too low!"
+  end
+  else
+    "Nailed it!"
+  end
+end
+
+def background_color(guess, number)
+  if guess > number
+    if guess > number + 5
+      "#ff0000"
+    else
+      "#ff6666"
+    end
+  elsif guess < number
+    if guess < number - 5
+      "#ff0000"
+    else
+      "#ff6666"
+    end
+  elsif guess == number
+    "green"
+  else
+    "red"
+  end
+end
+
 
 get '/' do
-  erb :index, :locals => {:number => num}
+  guess = params["guess"].to_i
+  message = check_guess(guess, number)
+  color = background_color(guess, number)
+  erb :index, :locals => {:number => number, :message => message, :color => color}
 end
+
